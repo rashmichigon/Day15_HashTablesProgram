f95cda50b7b1056d175c1f077f8ac3c7242852a0
Rashmi <rashmichigon65@gmail.com>
2022-12-01 15:20:43 +0530

    HashTableUC2

---
+++ HashTable.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Day15_HashTablesProgram
+{
+    internal class HashTable
+    {
+        // Function to count frequency of
+        // words in the given string
+        static void Count_freq(String str)
+        {
+            SortedDictionary<String,int> mp = new SortedDictionary<String, int>();
+
+            // Splitting to find the word
+            String[] arr = str.Split(' ');
+
+            // Loop to iterate over the words
+            for (int i = 0; i < arr.Length; i++)
+            {
+
+                // Condition to check if the
+                // array element is present
+                // the hash-map
+                if (mp.ContainsKey(arr[i]))
+                {
+                    mp[arr[i]] = mp[arr[i]] + 1;
+                }
+                else
+                {
+                    mp.Add(arr[i], 1);
+                }
+            }
+
+            // Loop to iterate over the
+            // elements of the map
+            foreach (KeyValuePair<String, int> entry in mp)
+            {
+                Console.WriteLine(entry.Key + " - " + entry.Value);
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,42 +1,18 @@
-﻿using System.Linq;
-using System;
-
-namespace Day15_HashTablesProgram
+﻿namespace Day15_HashTablesProgram
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main(String[] args)
         {
-            String str = "To be or not to be";
-            int[] freq = new int[str.Length];
-            int i, j;
+            String str = "Geeks For Geeks";
 
-            //Converts given string into character array  
-            char[] string1 = str.ToCharArray();
-
-            for (i = 0; i < str.Length; i++)
-            {
-                freq[i] = 1;
-                for (j = i + 1; j < str.Length; j++)
-                {
-                    if (string1[i] == string1[j])
-                    {
-                        freq[i]++;
-
-                        //Set string1[j] to 0 to avoid printing visited character  
-                        string1[j] = '0';
-                    }
-                }
-            }
-
-            //Displays the each character and their corresponding frequency  
-            Console.WriteLine("Characters and their corresponding frequencies");
-            for (i = 0; i < freq.Length; i++)
-            {
-                if (string1[i] != ' ' && string1[i] != '0')
-                    Console.WriteLine(string1[i] + "-" + freq[i]);
-            }
+            // Function call
+            Count_freq(str);
         }
 
+        private static void Count_freq(string str)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
