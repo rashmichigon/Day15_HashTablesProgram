ddb571934036f839ee3e5b37102455e3c5c37bf2
Rashmi <rashmichigon65@gmail.com>
2022-12-02 14:16:09 +0530

    HashTableUC3

--- Program.cs
+++ Program.cs
@@ -4,15 +4,15 @@
     {
         static void Main(String[] args)
         {
-            String str = "Geeks For Geeks";
+            String str = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
 
-            // Function call
-            Count_freq(str);
-        }
+            Console.WriteLine("Given String : " + str);
 
-        private static void Count_freq(string str)
-        {
-            throw new NotImplementedException();
+            // delete from index 5 to end of string
+            Console.WriteLine("New String1 : " + str.Remove(111));
+
+            // delete character from index 8 to end of string
+            //Console.WriteLine("New String2 : " + str.Remove(20));
         }
     }
 }
